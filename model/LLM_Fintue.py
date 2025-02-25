0b9496f743b2d7de5a39a2067d44c0445c53fc66
triple-purity <2024950583@qq.com>
2025-02-25 15:45:52 +0800

    third commit

--- model/LLM_Fintue.py
+++ model/LLM_Fintue.py
@@ -65,14 +65,13 @@ class MultiHeadAttention(nn.Module):
 class ReprogrammingLayer(nn.Module):
     def __init__(self, d_model, n_heads, d_llm, d_keys=None, attention_dropout=0.1):
         super(ReprogrammingLayer, self).__init__()
-
+        self.n_heads = n_heads
         d_keys = d_keys or (d_model // n_heads)
 
         self.query_projection = nn.Linear(d_model, d_keys * n_heads)
         self.key_projection = nn.Linear(d_llm, d_keys * n_heads)
         self.value_projection = nn.Linear(d_llm, d_keys * n_heads)
         self.out_projection = nn.Linear(d_keys * n_heads, d_llm)
-        self.n_heads = n_heads
         self.dropout = nn.Dropout(attention_dropout)
 
     def forward(self, target_embedding, source_embedding, value_embedding):
