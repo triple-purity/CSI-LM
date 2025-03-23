1e73b8e55880d8a857757ef5736cc3245fb4b0dd
Aoyun <13955277+aoyunzhang@user.noreply.gitee.com>
2025-03-24 00:06:28 +0800

    fifth commit

--- utils/train_util.py
+++ utils/train_util.py
@@ -13,5 +13,5 @@ def DomainDeception(domain_logits: torch.tensor, domain_labels: torch.tensor):
     domain_num = domain_logits.shape[1]
     soft_domain_logits = F.log_softmax(domain_logits, dim=1)
     domain_labels = F.one_hot(domain_labels, domain_num)
-    domain_loss = torch.sum(soft_domain_logits * domain_labels) / domain_labels.shape[0]
+    domain_loss = F.kl_div(soft_domain_logits, domain_labels, reduction='batchmean')
     return domain_loss
